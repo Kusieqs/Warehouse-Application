61577339a68b648835ae3e8a36230ba663aef304
Konrad <Konrad.kus@vp.pl>
2023-07-26 15:01:19 +0200

    Utils class

--- Warehouse Application/Product.cs
+++ Warehouse Application/Product.cs
@@ -1,90 +1,89 @@
 ﻿using System;
 using System.Text.RegularExpressions;
-namespace Warehouse_App
-{
-    public class Product
-    {
-        private string name;
-        private string id;
-        private double price;
-        private int quantity;
-        public DateTime date { get; set; }
+namespace Warehouse_Application;
 
-        public Product(string name, string id, double price, int quantity, DateTime date)
+public class Product
+{
+    private string name;
+    private string id;
+    private double price;
+    private int quantity;
+    public DateTime date { get; set; }
+
+    public Product(string name, string id, double price, int quantity, DateTime date)
+    {
+        if (Regex.IsMatch((id), @"^[A-Za-z]{4}\d{5}&") && price > 0 && quantity >= 0 && name.Length > 0)
         {
-            if (Regex.IsMatch((id), @"^[A-Za-z]{4}\d{5}&") && price > 0 && quantity >= 0)
+            this.id = id.Trim();
+            this.price = price;
+            this.quantity = quantity;
+        }
+        else
+            throw new FormatException("Informations about product are not correct");
+
+        this.name = name.Trim();
+        this.date = date;
+    }
+    public string Name
+    {
+        get
+        {
+            return name;
+        }
+        set
+        {
+            name = value.Trim();
+        }
+    }
+    public string Id
+    {
+        get
+        {
+            return id;
+        }
+        set
+        {
+            if (Regex.IsMatch((value), @"^[A-Za-z]{4}\d{5}&"))
             {
-                this.id = id.Trim();
-                this.price = price;
-                this.quantity = quantity;
+                id = value.Trim();
             }
             else
-                throw new FormatException("Informations about product are not correct");
-
-            this.name = name.Trim();
-            this.date = date;
+                throw new FormatException("Id is not correct");
         }
-        public string Name
-        {
-            get
-            {
-                return name;
-            }
-            set
-            {
-                name = value.Trim();
-            }
-        }
-        public string Id
-        {
-            get
-            {
-                return id;
-            }
-            set
-            {
-                if (Regex.IsMatch((value), @"^[A-Za-z]{4}\d{5}&"))
-                {
-                    id = value.Trim();
-                }
-                else
-                    throw new FormatException("Id is not correct");
-            }
-        }
-        public double Price
-        {
-            get
-            {
-                return price;
-            }
-            set
-            {
-                if (value > 0)
-                {
-                    price = value;
-                }
-                else
-                    throw new FormatException("Price is not correct");
-            }
-        }
-        public int Quantity
-        {
-            get
-            {
-                return quantity;
-            }
-            set
-            {
-                if (value >= 0)
-                {
-                    quantity = value;
-                }
-                else
-                    throw new FormatException("Quantity is not correct");
-            }
-        }
-
     }
+    public double Price
+    {
+        get
+        {
+            return price;
+        }
+        set
+        {
+            if (value > 0)
+            {
+                price = value;
+            }
+            else
+                throw new FormatException("Price is not correct");
+        }
+    }
+    public int Quantity
+    {
+        get
+        {
+            return quantity;
+        }
+        set
+        {
+            if (value >= 0)
+            {
+                quantity = value;
+            }
+            else
+                throw new FormatException("Quantity is not correct");
+        }
+    }
+
 }
 
 
---
+++ Warehouse Application/Utils.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Newtonsoft.Json;
+namespace Warehouse_Application
+{
+	public static class Utils
+	{
+		public static void FirstTimeUsing(ref List<Product> products,ref string systemOperation)
+		{
+			systemOperation = Path.Combine(systemOperation, "Products.json");
+
+			if (!File.Exists(systemOperation))
+			{
+				string jsonWriter = JsonConvert.SerializeObject("");
+				File.WriteAllText(systemOperation, jsonWriter);
+			}
+			else
+			{
+				string jsonReader = File.ReadAllText(systemOperation);
+				products = JsonConvert.DeserializeObject<List<Product>>(systemOperation);
+			}
+		}
+		public static void AddingProduct(ref List<Product> products)
+		{
+
+			bool correctPrice, correctQuantity, correctData = false;
+			string name, id;
+			int quantity;
+			double price;
+			DateTime date = DateTime.Now;
+
+			do
+			{
+				Console.Clear();
+				Console.Write("Name of product: ");
+				name = Console.ReadLine().Trim();
+
+				Console.Write("\nPrice of product: ");
+				correctPrice = double.TryParse(Console.ReadLine(), out price);
+
+				Console.Write("\nQuantity of product: ");
+				correctQuantity = int.TryParse(Console.ReadLine(), out quantity);
+
+				Console.Write("\nId of product (First 4 letters and 5 numbers, example - AbcD12345 )");
+				id = Console.ReadLine().Trim();
+
+				if(correctData && correctQuantity)
+				{
+					try
+					{
+						Product p1 = new Product(name, id, price, quantity, date);
+						correctData = true;
+						products.Add(p1);
+
+					}
+					catch(FormatException e)
+					{
+						Console.WriteLine(e.Message);
+					}
+					finally
+					{
+						Console.WriteLine("Click enter to continue");
+						Console.ReadKey();
+                    }
+				}
+				else
+				{
+					Console.WriteLine("Information about product are not correct\nClick enter to continue");
+					Console.ReadKey();
+				}
+			} while (!correctData);
+
+		}
+		public static void RaportOfProducts(List<Product> products)
+		{
+			bool endOfRaport = false;
+			do
+			{
+				Console.Clear();
+				Console.WriteLine("RAPORTS:");
+				Console.WriteLine("1.All Products\n2.Searching by infomration\n3.Sort by values\n4.Exit");
+				string answer = Console.ReadLine();
+				switch (answer)
+				{
+					case "1":
+						break;
+					case "2":
+						break;
+					case "3":
+						break;
+					case "4":
+						endOfRaport = true;
+						break;
+					default:
+						break;
+
+				}
+			} while (!endOfRaport);
+		}
+	}
+}
+
